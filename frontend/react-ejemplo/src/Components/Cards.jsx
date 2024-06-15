06c66d286905b3813bb57509d0dc4f6c896ceb5f
DidithJim <165655155+DidithJim@users.noreply.github.com>
2024-06-15 18:23:50 +0200

    Add files via upload

---
+++ frontend/react-ejemplo/src/Components/Cards.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+function Cards() {
+
+  const [Cards, setCards] = useState([]);
+ 
+
+  useEffect(() => {
+    let controller = new AbortController();
+    let options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      signal: controller.signal,
+    };
+
+    fetch(`${import.meta.env.VITE_API}/api/content`,options)
+    .then(res=>res.json())
+    .then(data=>setCards(data.slice(5)))
+    .catch(err=>console.log(err))
+
+    
+  }, []);
+
+
+  return (
+    <section className='seccion__cards'>
+      <div className='seccion__cards--contenedor'>
+        {Cards.map(card => (
+          <div key={card.id} className='card' style={{ backgroundColor: card.color }}>
+            <img src={card.image} alt={`Card ${card.id}`} className='card__image' />
+            <p>{card.text}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+  
+
+export default Cards;
---
+++ frontend/react-ejemplo/src/Components/Testimonials.jsx
@@ -0,0 +1,47 @@
+import { useState,useEffect } from "react";
+
+function Testimonials () {
+    const [testimonials,setTestimonials] = useState([]);
+
+
+    useEffect(()=>{
+
+        let controller = new AbortController();
+        let options = {
+            method : 'GET',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            signal : controller.signal
+        };
+
+        fetch(`${import.meta.env.VITE_API}/api/testimonials`, options)
+        .then(res=>res.json())   
+        .then(data=>setTestimonials(data))
+        .catch(err=>console.log(err))     
+    },[])
+
+
+
+   
+    return (
+        <section className="seccion__testimonials">
+            <h2 className='seccion__testimonials--titulo'>Praise for Triage 1</h2>
+            <div className="seccion__testimonials--contenedor">
+                {testimonials.map((testimonial) => ( 
+                    <div key={testimonial.id} className="testimonial">
+                        <p>{testimonial.text}</p>
+                        <img src={testimonial.image} alt={`Imagen Testimonials ${testimonial.id}`} className="testimonial__image"  />
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+
+
+}
+
+
+export default Testimonials;
+
+
